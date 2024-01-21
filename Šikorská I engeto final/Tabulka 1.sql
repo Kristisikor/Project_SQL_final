7715ab7e6ab62f7ad3f305045f803fb6e635ad6a
Kristína Šikorská <kristina.sikorsky@gmail.com>
2024-01-21 15:37:25 +0100

    opravena primarni tabulka

--- Šikorská I engeto final/Tabulka 1.sql
+++ Šikorská I engeto final/Tabulka 1.sql
@@ -1,37 +1,36 @@
--- zdravechalko --
-
+-- zdravechalko
 CREATE TABLE t_Kristina_Sikorska_project_SQL_primary_final AS
-SELECT 
-    cp.payroll_year AS year,
-    cp.value AS wage,
-    cp2.value AS price, 
-    cpib.name AS industry,
-    cpc.name AS category
-FROM 
-    czechia_payroll cp
-JOIN 
-    czechia_payroll_industry_branch cpib 
+WITH wage_avg AS (
+    SELECT 
+    	cp.payroll_year AS year, 
+    	cpib.name AS industry, 
+    	AVG(cp.value) AS wage
+    FROM czechia_payroll cp
+    JOIN czechia_payroll_industry_branch cpib 
     ON cp.industry_branch_code = cpib.code
-JOIN 
-    czechia_price cp2
-	ON YEAR(cp2.date_from) = cp.payroll_year 
-    AND cp.value_type_code = 5958
-JOIN 
-    czechia_price_category cpc 
+    WHERE cp.value_type_code = 5958
+    GROUP BY cp.payroll_year, cpib.name
+), price_avg AS (
+    SELECT 
+    	YEAR(cp2.date_from) AS year, 
+    	cpc.name AS category, 
+    	ROUND(AVG(cp2.value), 2) AS price
+    FROM czechia_price cp2
+    JOIN czechia_price_category cpc 
     ON cp2.category_code = cpc.code
-GROUP BY cp.payroll_year, cpib.name, cpc.name     
-    
+    GROUP BY YEAR(cp2.date_from), cpc.name
+)
 SELECT 
-	year, 
-	industry, 
-	wage, 
-	category, 
-	price
-FROM t_Kristina_Sikorska_project_SQL_primary_final;    
- 
-
+	w.year, 
+	w.industry, 
+	w.wage, 
+	p.category, 
+	p.price
+FROM wage_avg w
+JOIN price_avg p ON w.year = p.year;
   
-    
+SELECT *
+FROM t_Kristina_Sikorska_project_SQL_primary_final 
     
     
     
